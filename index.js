106061f200b87878a9c5ab51b400ab05d4d1875d
Klein <klein.gachuhi@student.moringaschool.com>
2025-03-10 15:01:43 +0300

    Changes

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// console.log(multiply(31, 2));
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+const max = (1, 2, 3, 5, 20)
+ console.log(Math.max(max));
+
+let num3 = 24;
+let num4 = 5;
+ 
+const mod = 24 % 5 ;
+console.log(mod);
+
+const random = 4;
+console.log(random);
